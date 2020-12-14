c379a7c97ef155612ebcfa8019d8c95319c69d36
Jesper Lundborg <jesper.lundborg@student.kyh.se>
2020-12-14 14:22:39 +0100

    Final commit!

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,24 +1,15 @@
 package com.company;
 import org.eclipse.paho.client.mqttv3.*;
-import org.eclipse.paho.client.mqttv3.internal.wire.MqttReceivedMessage;
-import org.eclipse.paho.client.mqttv3.internal.wire.MqttSubscribe;
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence;
-
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
 public class Main {
-
-
     public static void main(String[] args) {
-
         String topic        = "temperature-topic/1";
-        String topicTwo     = "temperatureCalc-topic/2";
-        String content      = " ";
-        int qos             = 2;
+        String topicTwo     = "control-topic/2";
         String broker       = "tcp://broker.hivemq.com:1883";
         String clientId     = "clientId-Jesper";
         MemoryPersistence persistence = new MemoryPersistence();
@@ -29,30 +20,20 @@ public class Main {
             System.out.println("Connecting to broker: "+ broker);
             client.connect(connOpts);
             System.out.println("Connected");
-            FileWriter log = new FileWriter("log.txt");
+            System.out.println("Subscribing to topics: " + topic + " and " + topicTwo);
+            FileWriter log = new FileWriter("Log.txt");
             client.subscribe(topic, (topic1, message) -> {
-                System.out.println(message.toString());
+                System.out.println("Adding to log: " + message.toString() + ", from " + topic);
                 String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
                 log.write("\n" + date + " - " + topic + " - " + message.toString());
-                //log.write("\n" + date + " - " + topicTwo + " - " + message.toString()+ "\n");
                 log.flush();
             });
             client.subscribe(topicTwo, (topic1, message) -> {
-                System.out.println(message.toString());
+                System.out.println("Adding to log: " + message.toString() + ", from " + topicTwo);
                 String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-                //log.write("\n" + date + " - " + topic + " - " + message.toString());
                 log.write("\n" + date + " - " + topicTwo + " - " + message.toString()+ "\n");
                 log.flush();
             });
-            MqttMessage response = new MqttMessage(content.getBytes());
-            response.setQos(qos);
-            client.publish(topic, response);
-            System.out.println("Message published");
-
-
-            //sampleClient.disconnect();
-            //System.out.println("Disconnected");
-            //System.exit(0);
         } catch(MqttException me) {
             System.out.println("reason " + me.getReasonCode());
             System.out.println("msg " + me.getMessage());
@@ -63,6 +44,5 @@ public class Main {
         } catch (IOException e ){
             System.out.println("fel");
         }
-
     }
 }
