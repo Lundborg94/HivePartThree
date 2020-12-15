2d1905eb8b8db6b9b849520679a4c2ded4227f83
JohanWWW <42637697+JohanWWW@users.noreply.github.com>
2020-12-15 23:14:24 +0100

    set clean state to false and fix logging bugs

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,28 +12,30 @@ public class Main {
         String topicTwo     = "control-topic/2";
         String broker       = "tcp://broker.hivemq.com:1883";
         String clientId     = "clientId-Jesper";
-        MemoryPersistence persistence = new MemoryPersistence();
         try {
-            MqttClient client = new MqttClient(broker, clientId, persistence);
+            MqttClient client = new MqttClient(broker, clientId, new MemoryPersistence());
             MqttConnectOptions connOpts = new MqttConnectOptions();
-            connOpts.setCleanSession(true);
-            System.out.println("Connecting to broker: "+ broker);
+            connOpts.setCleanSession(false);
+            System.out.println("Connecting to broker: " + broker);
             client.connect(connOpts);
             System.out.println("Connected");
             System.out.println("Subscribing to topics: " + topic + " and " + topicTwo);
-            FileWriter log = new FileWriter("Log.txt");
-            client.subscribe(topic, (topic1, message) -> {
+            FileWriter log = new FileWriter("Log.txt", true);
+
+            client.subscribe(topic, 2, (t, message) -> {
                 System.out.println("Adding to log: " + message.toString() + ", from " + topic);
                 String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-                log.write("\n" + date + " - " + topic + " - " + message.toString());
+                log.write(date + " - " + topic + " - " + message.toString() + "\n");
                 log.flush();
             });
-            client.subscribe(topicTwo, (topic1, message) -> {
+
+            client.subscribe(topicTwo, 2, (t, message) -> {
                 System.out.println("Adding to log: " + message.toString() + ", from " + topicTwo);
                 String date = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
-                log.write("\n" + date + " - " + topicTwo + " - " + message.toString()+ "\n");
+                log.write(date + " - " + topicTwo + " - " + message.toString()+ "\n");
                 log.flush();
             });
+
         } catch(MqttException me) {
             System.out.println("reason " + me.getReasonCode());
             System.out.println("msg " + me.getMessage());
